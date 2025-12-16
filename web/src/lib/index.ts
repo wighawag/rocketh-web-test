1c74b7480923e6785399570ea9260ff514e54cac
wighawag <wighawag@gmail.com>
2025-12-16 11:18:27 +0000

    @rocketh/web

--- web/src/lib/index.ts
+++ web/src/lib/index.ts
@@ -1,13 +1,10 @@
 import { Provider } from '@remix-project/remix-simulator';
-import { createExecutor, resolveConfig, resolveExecutionParams } from '@rocketh/core';
-import type { DeploymentStore, PromptExecutor } from '@rocketh/core/types';
-import { hardhat } from 'viem/chains';
-
 import { privateKeyToAccount } from 'viem/accounts';
-
 import DeployScript from 'template-ethereum-contracts/deploy/001_deploy_greetings_registry.js';
 import { config } from 'template-ethereum-contracts/rocketh/config.js';
-import { createEmptyDeploymentStore } from './deployment-store';
+
+import { setupEnvironment } from '@rocketh/web';
+import { extensions } from 'template-ethereum-contracts/rocketh/deploy.js';
 
 const account0 = privateKeyToAccount(
 	'0x503f38a9c967ed597e47fe25643985f032b072db8075426a92110f82df48dfcb'
@@ -22,34 +19,15 @@ export async function test() {
 	const provider = new Provider({});
 	await provider.init();
 
-	const deploymentStore: DeploymentStore = createEmptyDeploymentStore();
+	const { loadAndExecuteDeploymentsFromModules } = setupEnvironment(config, extensions);
 
-	const promptExecutor: PromptExecutor = async () => {
-		return {
-			proceed: true
-		};
-	};
-
-	const executor = createExecutor(deploymentStore, promptExecutor);
-
-	const resolvedConfig = resolveConfig(config);
-	const executionParams = resolveExecutionParams(resolvedConfig, {}, hardhat.id);
-
-	// TODO
-	// fix import from rocketh
 	// fix deterministic
 	// add option of proxy to not have implementation use deterministic
 	// add "./rocketh/*": "./dist/rocketh/*", to template-ethereum-contracts exports
 
-	const env = await executor.executeDeployScriptModules(
-		[{ id: '1', module: DeployScript }],
-		resolvedConfig,
-		{
-			...executionParams,
-			provider: provider as any,
-			logLevel: 6
-		}
-	);
+	const env = await loadAndExecuteDeploymentsFromModules([{ id: '1', module: DeployScript }], {
+		provider: provider as any
+	});
 
 	console.log(env);
 }
